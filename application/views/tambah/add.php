94823a28226b4042430fc03644507255fd6b09b3
Candia Gus Prihatama <96465620+Candia20@users.noreply.github.com>
2022-01-04 19:13:43 +0700

    Add files via upload

---
+++ application/views/tambah/add.php
@@ -0,0 +1,53 @@
+<div class="container-fluid">
+    <h4>Tambah</h4>
+
+    <hr>
+    <br>
+    <form>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">ID</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Nama Kegiatan</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Tanggal</label>
+            <br>
+            <input type="date" name="">
+        </div>
+        <br>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Waktu</label>
+            <br>
+            <input type="time" name="">
+        </div>
+        <br>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Stakeholder</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+        <br>
+        <div class="input-group mb-3">
+            <span class="input-group-text">Rp</span>
+            <input type="text" class="form-control" aria-label="Dollar amount (with dot and two decimal places)">
+        </div>
+        <br>
+        <div class="input-group mb-3">
+            <label class="input-group-text" for="inputGroupFile02">LPJ</label>
+            <input type="file" class="form-control" id="inputGroupFile02">
+        </div>
+        <br>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Status</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+        <br>
+        <div class="btn-group" role="group" aria-label="Basic example">
+            <button type="button" class="btn btn-primary">Save</button>
+            <button type="button" class="btn btn-primary">Cancel</button>
+        </div>
+
+    </form>
+</div>
