59d67d932b2610dc527e1a50aadaf824a7c8e288
Candia Gus Prihatama <96465620+Candia20@users.noreply.github.com>
2022-01-04 19:13:05 +0700

    Add files via upload

---
+++ application/views/himpunan/index.php
@@ -0,0 +1,27 @@
+
+<body>
+
+
+    <div class="container-fluid">
+        <div class="row content">
+            <div class="col-sm-3 sidenav">
+                <h4>MENU</h4>
+                <ul class="nav nav-pills nav-stacked">
+                    <li class="active"><a href="#section1">Himpunan</a></li>
+                    <li><a href="#section2">Edit</a></li>
+                </ul><br>
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Search Blog..">
+                    <span class="input-group-btn">
+                        <button class="btn btn-default" type="button">
+                            <span class="glyphicon glyphicon-search"></span>
+                        </button>
+                    </span>
+                </div>
+            </div>
+
+            <div class="col-sm-9">
+                <h4><small>Himpunan</small></h4>
+                <hr>
+                <h2>Kegiatan</h2>
+ 
