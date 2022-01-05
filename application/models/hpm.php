ee36e50718cec8cbd20e8e46569345f82e167f89
Candia Gus Prihatama <96465620+Candia20@users.noreply.github.com>
2022-01-05 17:42:28 +0700

    Delete hpm.php

--- application/models/hpm.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-class hpm extends CI_Model {
-    public function semuadata() {
-        return $this->db->get('himpunan')->result_array();
-    }
-}
